5d6694a24906836df63d3615347574b057c5c609
Josh Waller <mdx.programs@gmail.com>
2019-05-12 13:17:18 -0400

    Adding static build

--- fastic.py
+++ fastic.py
@@ -1,5 +1,8 @@
+import sys
+
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -8,6 +11,7 @@ FLATPAGES_EXTENSION = ".md"
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+freezer = Freezer(app)
 
 
 @app.route('/')
@@ -23,4 +27,7 @@ def page(path):
 
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        freezer.freeze()
+    else:
+        app.run(port=3000)
